b54790593a3fce2ae52f451b32144fdf1a53a34a
Abdullah Şahin <sahin.abdullah1986@gmail.com>
2026-05-24 13:07:29 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,34 +1,17 @@
-const CACHE_NAME = 'coordinate-go-v2';
-const ASSETS_TO_CACHE = [
-  './',
-  './index.html',
-  './manifest.json',
-  './apple-touch-icon.png',
-  './icon-192x192.png',
-  './icon-512x512.png'
-];
+﻿const CACHE_CLEANER_NAME = 'coordinate-go-cache-cleaner';
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
-  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-    ))
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', event => {
-  const url = new URL(event.request.url);
-  if (url.origin !== self.location.origin) return;
-
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.map(key => caches.delete(key)));
+    const clientsList = await self.clients.matchAll({ type: 'window', includeUncontrolled: true });
+    await self.registration.unregister();
+    clientsList.forEach(client => {
+      if (client.url) client.navigate(client.url);
+    });
+  })());
 });
